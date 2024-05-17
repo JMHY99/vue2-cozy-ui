a6d10d425a8af5edcd640a48c18aae162a11fc23
猫小九 <2935229453@qq.com>
2024-05-17 16:16:00 +0800

    修改颜色

---
+++ packages/layout/index.js
@@ -0,0 +1,7 @@
+import CLayout from "./layout.vue";
+
+CLayout.install = function (Vue) {
+  Vue.component(CLayout.name, CLayout);
+};
+
+export default CLayout;
---
+++ packages/layout/layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CLayout",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
